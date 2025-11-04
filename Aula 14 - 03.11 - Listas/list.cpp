1f18d43ca7119780dd002da8e601126b8f95f442
Diwei lci <diwei.dvn@gmail.com>
2025-11-03 22:00:17 -0300

    atualizando funcao print

--- Aula 14 - 03.11 - Listas/list.cpp
+++ Aula 14 - 03.11 - Listas/list.cpp
@@ -4,11 +4,12 @@
 
 namespace UPF{
 
-    struct node{
-        std::string value; //VALOR
-        node *previous = nullptr; //Nodo anterior
-        node *next = nullptr; //Nodo posterior
-    };
+struct node{
+    std::string value; //VALOR
+    node *previous = nullptr; //Nodo anterior
+    node *next = nullptr; //Nodo posterior
+};
+
 struct list{
     //Variaveis de controle
     node *first = nullptr; //primeiro nodo
@@ -31,6 +32,28 @@ struct list{
         first = p;
         if(last == nullptr) //Lista vazia
             last = p;
+    }
+
+    void push_back(const std::string &s){
+        num_nodes++;
+        node *p = new node; //aloca memoria 
+        p->value = s;
+        p->next = nullptr;
+        p->previous = last;
+        if(last != nullptr) //Não é nulo, já existe elementos
+            last->next = p;
+        last = p;
+        if(first == nullptr) //Em caso de lista ainda vazia. Alé, do ultimo, será também o primeiro
+            first = p;
+    }
+
+    void print(){
+        node *p = first; //Pega o valor do primeiro nodo
+        while(p!=nullptr){
+            std::cout << '"' << p->value << '"' << " ";
+            p = p->next;
+            std::cout << std::endl;
+        }
     }
 };
 
